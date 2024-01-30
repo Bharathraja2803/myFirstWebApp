4f29dd65202892313066673cbeab945af0b7a510
Bharathraja2803 <bharathrajakrishnan@gmail.com>
2024-01-30 13:58:32 +0530

    after_simple_hello_controller

---
+++ src/main/java/com/in28minutes/springboot/myFirstWebApp/hello/SayHelloController.java
@@ -0,0 +1,29 @@
+package com.in28minutes.springboot.myFirstWebApp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+	
+	@RequestMapping("say-hello-html")
+	@ResponseBody
+	public String sayHelloHtml() {
+		String htmlTags = """
+				<html>
+				<head>
+				<title>My first HTML Page</title>
+				<h1>My first html page with body</h1>
+				</head>
+				</html>
+				""";
+		return htmlTags;
+	}
+}
